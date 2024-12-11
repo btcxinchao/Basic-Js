d0a54fd8d18b5aae16360e94db03078e0c65ab2c
chuong <buitanchuongg@gmail.com>
2024-12-11 21:49:07 +0700

    chapter 2 video8

---
+++ video8/video8.js
@@ -0,0 +1,12 @@
+console.log(`hello word`)
+
+//Object
+let obj = {
+    name : 'chuong',
+    address:  'BS',
+    email : "buitanchuong@gmail.com"
+};
+//Thay đổi tên 
+ obj.name = 'Bui Tan Chuong'
+
+console.log("what is your name ? \n ", `My name is : `,obj.name );
