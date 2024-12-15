5f820aad4903cb1ffea62b98da57b9a85ceb7182
chuong <buitanchuongg@gmail.com>
2024-12-15 11:57:14 +0700

    chapter 2 video13

---
+++ video13/video13.js
@@ -0,0 +1,12 @@
+let arrtop3 = ['Mu', 'Chelse', 'Liver'];
+
+let i = 0;
+while (i < arrtop3.length) {
+    if (arrtop3[i] === 'Chelse') {
+        console.log('found it top 2 : ', arrtop3[i]);
+        break;
+    }
+    console.log('check value i : ', i);
+
+    i++;
+}
--- video8/video8.js
+++ video8/video8.js
@@ -1,12 +1,38 @@
-console.log(`hello word`)
 
-//Object
-let obj = {
-    name : 'chuong',
-    address:  'BS',
-    email : "buitanchuong@gmail.com"
-};
-//Thay đổi tên 
- obj.name = 'Bui Tan Chuong'
+// null , empty , Undefined 
+let a = 5
+let b = 5
+console.log('so sánh a== b ', 'a= ', a, 'b=', b, 'result', a == b);
+console.log('so sánh a=== b ', 'a= ', a, 'b=', b, 'result', a === b);
 
-console.log("what is your name ? \n ", `My name is : `,obj.name );
+//Vòng FOR
+let arr = ['MU', 'CHELSE', 'MC'];
+for (let i = 0; i < arr.length; i++) {
+    console.log('check varable i=', arr[i]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+//console.log('check varable i:' +i); dùng var chạy hết code thì sẽ chạy được
+
+// i++ <=>i = i +1  thực hiện biến i xong mới tăng giá trị
+
+
+
+//++i <=> i+1 = i        tăng giá trị rồi mới thực hiện biến i
+//i = 1
+//j = ++i => i=2 ; j= 2 
